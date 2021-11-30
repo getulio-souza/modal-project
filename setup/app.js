b820a9ceaeed0a5ad8d3bb57cac7d34efd050c0e
getulio-souza <76955711+getulio-souza@users.noreply.github.com>
2021-11-30 15:42:26 -0300

    Add files via upload

---
+++ setup/app.js
@@ -0,0 +1,22 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+// const modalBtn = document.querySelector('.modal-btn');
+// const modalOverley = document.querySelector('.modal-overlay');
+// const closeBtn = document.querySelector('.close-btn');
+
+// modalBtn.addEventListener('click', function(){
+//     if(modalOverley.classList.contains = "modal-btn"){
+//         modalOverley.classList.add('open-modal');
+//     }
+// })
+
+// closeBtn.addEventListener('click', function(){
+//     if(modalOverley.classList.contains = "modal-btn"){
+//         modalOverley.classList.remove('open-modal');
+//     }
+// })
+
+
